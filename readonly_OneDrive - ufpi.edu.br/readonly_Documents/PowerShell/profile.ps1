62062111aa3b781a11233f81d94e96e150b8a525
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2024-09-13 14:32:09 -0300

    scoop

--- readonly_OneDrive - ufpi.edu.br/readonly_Documents/PowerShell/profile.ps1
+++ readonly_OneDrive - ufpi.edu.br/readonly_Documents/PowerShell/profile.ps1
@@ -1,8 +1,8 @@
 
 #region mamba initialize
 # !! Contents within this block are managed by 'mamba shell init' !!
-$Env:MAMBA_ROOT_PREFIX = "C:\Users\Lucio Araujo\micromamba"
-$Env:MAMBA_EXE = "C:\Users\Lucio Araujo\AppData\Local\micromamba\micromamba.exe"
+$Env:MAMBA_ROOT_PREFIX = "C:\Users\Lu\micromamba"
+$Env:MAMBA_EXE = "C:\Users\Lu\AppData\Local\micromamba\micromamba.exe"
 (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -p $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 #endregion
 
