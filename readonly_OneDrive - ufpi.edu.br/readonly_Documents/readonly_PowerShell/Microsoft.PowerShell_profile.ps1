6e9b3ac91333a33852f087ff56de29e7d9034567
Lucio Araujo <lucio_a.khfzo@aleeas.com>
2025-06-13 15:20:30 -0300

    A_Lot

--- readonly_OneDrive - ufpi.edu.br/readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive - ufpi.edu.br/readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -91,6 +91,7 @@ Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 #Carapace
+$env:CARAPACE_BRIDGES = 'inshellisense'
 Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 carapace _carapace | Out-String | Invoke-Expression
